81d0c2e0e25e31ab6f515c3701102e4fedf9b836
antbeltran <anthonybltrn@gmail.com>
2026-04-21 15:44:31 -0500

    outputted different formats need to change the time to specific timezone

--- src/main/java/com/pluralsight/formatDates.java
+++ src/main/java/com/pluralsight/formatDates.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class formatDates {
+
+
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String formattedDate = currentDate.format(formatter);
+        System.out.println(formattedDate);
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(currentDate.format(formatter1));
+        System.out.println(currentDate.getDayOfWeek() + " " + currentDate.getDayOfMonth() + ", " + currentDate.getYear());
+        System.out.println(currentDate.getDayOfWeek() + " " + currentDate.getDayOfMonth() + ", " + currentDate.getYear() + "  " + currentTime.getHour() + ":" + currentTime.getMinute());
+
+
+
+    }
 }
